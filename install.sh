c4c24b2a3c67461ccded4eb8afee1b1ea62c7d41
DSuite Tecnologia <admin@dsuite.com.br>
2021-02-04 19:27:54 -0300

    Ajustes

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
+sudo apt install unixodbc-dev -y
 pip3 install -r /home/pi/dtrack_gateway/requirements.txt
 sudo cp /home/pi/dtrack_gateway/dtrack_gateway.service /etc/systemd/system/dtrack_gateway.service
+sudo systemctl daemon-reload
 sudo systemctl enable dtrack_gateway.service
 sudo systemctl start dtrack_gateway.service
