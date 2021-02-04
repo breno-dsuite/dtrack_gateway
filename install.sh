eeb477439749ccd60796364bf0f0e2c54024d26e
Breno Uchôa - DSuite <47765861+breno-dsuite@users.noreply.github.com>
2021-02-04 19:36:44 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt install unixodbc-dev -y
-pip3 install -r /home/pi/dtrack_gateway/requirements.txt
+sudo pip3 install -r /home/pi/dtrack_gateway/requirements.txt
 sudo cp /home/pi/dtrack_gateway/dtrack_gateway.service /etc/systemd/system/dtrack_gateway.service
 sudo systemctl daemon-reload
 sudo systemctl enable dtrack_gateway.service
