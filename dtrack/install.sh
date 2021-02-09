3187bcea0183bca8f90b13b041335d37b19265ad
Breno Uchôa <brenouchoa@gmail.com>
2021-02-08 22:46:21 -0300

    pip ubuntu

--- dtrack/install.sh
+++ dtrack/install.sh
@@ -1,4 +1,4 @@
-sudo apt install unixodbc-dev -y python-usb python3-usb
+sudo apt install unixodbc-dev -y python-usb python3-usb python3-pip
 sudo pip3 install -r /home/dtrack/dtrack_gateway/requirements.txt
 sudo cp /home/dtrack/dtrack_gateway/dtrack/dtrack_gateway.service /etc/systemd/system/dtrack_gateway.service
 sudo systemctl daemon-reload
